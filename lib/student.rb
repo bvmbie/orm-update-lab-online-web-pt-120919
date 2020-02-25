dba187146f19e2318849b4706cbddb1d103554d9
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-25 13:14:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,8 @@ class Student
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      
   end
 
 end
