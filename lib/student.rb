f3a63223e43dcad63e8194bcae18a377101e17f1
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-25 12:55:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
   
   def self.create_table
-    sql = "CREATE TABLE students VALUES id INTEGER PRIMARY KEY, name TEXT, grade INTEGER"
+    sql = "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)"
     DB[:conn].execute(sql)
   end
 
